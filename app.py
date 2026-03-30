67852c1a0170a5e99013efb8af9331e9c908ee3b
Ashkesh-D <ashlesh.deshmukh.sg@gmail.com>
2026-03-30 23:01:52 +0800

    login page form made, rule checking wip

--- app.py
+++ app.py
@@ -1,11 +1,39 @@
+import os
+import dotenv
+from dotenv import load_dotenv
 import flask
 from flask import Flask, render_template
-
+import firebase_admin
+from firebase_admin import credentials, firestore
+load_dotenv()
 app = Flask(__name__)
+
+firebase_dict = {
+    "type": os.getenv("FIREBASE_TYPE"),
+    "project_id": os.getenv("FIREBASE_PROJECT_ID"),
+    "private_key_id": os.getenv("FIREBASE_PRIVATE_KEY_ID"),
+    "private_key": os.getenv("FIREBASE_PRIVATE_KEY").replace("\\n", "\n"),
+    "client_email": os.getenv("FIREBASE_CLIENT_EMAIL"),
+    "client_id": os.getenv("FIREBASE_CLIENT_ID"),
+    "auth_uri": os.getenv("FIREBASE_AUTH_URI"),
+    "token_uri": os.getenv("FIREBASE_TOKEN_URI"),
+    "auth_provider_x509_cert_url": os.getenv("FIREBASE_AUTH_PROVIDER_CERT_URL"),
+    "client_x509_cert_url": os.getenv("FIREBASE_CLIENT_CERT_URL"),
+    "universe_domain": os.getenv("FIREBASE_UNIVERSE_DOMAIN")
+}
+
+cred = credentials.Certificate(firebase_dict)
+firebase_admin.initialize_app(cred)
+db = firestore.client()
+
 
 @app.route("/")
 def home():
     return render_template("index.html")
+
+@app.route("/auth")
+def auth():
+    return render_template("auth.html")
 
 if __name__ == "__main__":
     app.run(debug=True, port=6969)
